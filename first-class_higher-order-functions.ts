bf2f2c157ecbeb424a37d5055aad2e785f510003
Carlos Quesada <carlosquesadaumana@gmail.com>
2022-02-19 15:10:50 -0600

    File filled

---
+++ first-class_higher-order-functions.ts
@@ -0,0 +1,49 @@
+interface Videogame {
+                name: string;
+                rating: number
+};
+
+let data: Videogame[] = [
+                {
+                                name: 'God of War Ragnarok',
+                                rating: 4.5
+                },
+                {
+                                name: 'Destiny 2: La reina bruja',
+                                rating: 4.1
+                },
+                {
+                                name: 'Horizon: Forbidden West',
+                                rating: 4.3
+                }
+];
+
+// getInfo is a higher order function
+// fn if a first class function
+
+function getInfo(
+                videogames: Videogame[], 
+                fn: (b: string[]) => void)
+{
+                const videogamesInfo = videogames.map( element => `Videojuego: ${ element.name } con una valoración de: ${ element.rating }`);  
+                
+                fn( videogamesInfo )
+};
+
+function show( videogames: string[] ) {
+                videogames.forEach( item => console.log( item ) )
+};
+
+function showToUpper( videogames: string[] ) {
+                videogames.forEach( item => console.log( item.toUpperCase() ) )
+};
+
+function showOrdered( videogames: string[] ) {
+                videogames.sort().forEach( ( item, index ) => {
+                                console.log(`${ index + 1 } - ${ item }`);
+                } );
+}
+
+getInfo( data, show )
+getInfo( data, showToUpper )
+getInfo( data, showOrdered )
